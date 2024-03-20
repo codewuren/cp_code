db74c418803cd8eb75c50e32a07c42388fad6284
codewuren <codewuren@gmail.com>
2024-03-21 00:01:49 +0800

    finish codeforces 263-A

--- codeforces/problemset/263-A.cc
+++ codeforces/problemset/263-A.cc
@@ -6,6 +6,20 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    
+    int x, y;
+    vector<vector<int>> matrix(5, vector<int>(5));
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> matrix[i][j];
+            if (matrix[i][j] == 1) {
+                x = i;
+                y = j;
+            }
+        }
+    }
+
+    // cout << "x = " << x << " y = " << y << endl;
+    cout << abs(y - 2) + abs(x - 2) << endl;
+
     return 0;
 }
